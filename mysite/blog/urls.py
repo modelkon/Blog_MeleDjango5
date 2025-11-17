90da3ccb3ea6cd723d4c9a3afb3854fd52689488
Konrad Modelski <76629128+modelkon@users.noreply.github.com>
2025-11-17 19:26:00 +0000

    Dodanie stronicowania

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,14 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     #widoki posta
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path(
+        '',
+        views.post_list,
+        name='post_list'
+    ),
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'
+    ),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,24 +1,32 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.http import Http404
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    #stronicowanie z 3 postami na stronę
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     """try:
         post = Post.published.get(id=id)
     except Post.DoesNotExist:
         raise Http404("Nie znaleziono posta mordko.")"""
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
     return render(
         request,
